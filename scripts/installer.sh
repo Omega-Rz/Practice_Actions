8905da2815afa8716dfde8d11a06cdda34141a8e
Omega-Rz <riteshzopeofficial17@gmail.com>
2025-07-21 10:55:31 +0530

    checking repo lvl secrets

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+password=secret123
+if [[ "$1" == "$password" ]]; then
+    echo "authenticated"
+else
+    echo "authentication failed"
+    exit 1
+fi
 sudo apt install figlet
 figlet "GH200" >> abc.txt
 sleep 5
